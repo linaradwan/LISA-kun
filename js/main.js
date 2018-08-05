17aaf69764571e67d0ba8a93d68a56ce1e46c5c5
Safery Hossain <Mohammed.Hossain1@ibm.com>
2018-08-04 22:57:12 -0400

    added the ability to complete auto result of a search text
    Need to further enable the full feature for all manga title.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 function showTop(str) {
         xmlhttp = new XMLHttpRequest();
-       
+
         xmlhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
                 document.getElementById(" ").innerHTML = this.responseText;
@@ -10,4 +10,13 @@ function showTop(str) {
         xmlhttp.open("GET","getTop.php?q="+str,true);
         xmlhttp.send();
     }
-}
+
+var allMangaTitle = [
+    "Naruto",
+    "Naruto Shippuden",
+    "Naruto: Movie 1",
+    "Naruto: The ninja return",
+    "Who is Naruto?"];
+$("#mangaSearchFieldBox").autocomplete({
+    source: allMangaTitle
+});
