160dc91f51c811fa70c13cecc2b37b3047663eb5
Safery Hossain <janingpower@yahoo.com>
2018-09-09 14:52:11 -0400

    Added automation of manga generate for front page. Added the ability to show "not found image" when a manga image does not exist on the mangaEden DB

--- js/main.js
+++ js/main.js
@@ -19,10 +19,14 @@ $( document ).ready(function() {
       dataType: "json",
       url: "https://www.mangaeden.com/api/list/0/",
       success: function(data) {
+          mangaAPI = data;
           //console.log(data['manga'][0]["t"]);
           for (var i = 0; i < data['manga'].length; i++) {
                allMangaTitle.push(data['manga'][i]["t"]);
            }
+
+           // Generate the manga selection for front page
+           generateFrontPageManga(data);
         }
     });
 
@@ -38,6 +42,7 @@ $( document ).ready(function() {
             // LINA PUT THE FUNCTION THAT GENERATES THE SEARCH RESULTS BELOW HERE!!!
         }
     });
+
 });
 
 function searchResult(){
@@ -51,4 +56,35 @@ function searchResult(){
 
 function clearAllPreviousSearchResult(){
   $("#allSearchResultDiv").html("");
+}
+
+function generateFrontPageManga(data){
+  lengthOfAPI = data['manga'].length;
+  //4e70ea10c092255ef7004aa2, 4e70ea03c092255ef70046f0, 4e70ea10c092255ef7004a8c, 5224e32c45b9effc5b1efec9, 4e70ea01c092255ef7004674, 4e70ea03c092255ef7004734
+  for (var i = 0; i < lengthOfAPI; i++) {
+    if (['4e70ea10c092255ef7004aa2', '4e70ea03c092255ef70046f0', '4e70ea10c092255ef7004a8c', '5224e32c45b9effc5b1efec9', '4e70ea01c092255ef7004674', '4e70ea03c092255ef7004734'].indexOf(data['manga'][i]["i"]) >= 0) {
+      createMangaForTopPick(data['manga'][i]);
+    }
+  }
+
+  counter = 0;
+  while (counter < 6){
+    createMangaForRandomPick(data['manga'][Math.floor(Math.random() * lengthOfAPI) + 0]);
+    counter++;
+  }
+}
+
+function createMangaForTopPick(mangaDetail){
+    var element = document.getElementById('mainContent');
+    element.insertAdjacentHTML('afterbegin', "<div class='col-md-4'> <div class='card mb-4 box-shadow'><a href='pages/manga.html?mangaID=" + mangaDetail['i'] + "'><center><img class='card-img-top' src='https://cdn.mangaeden.com/mangasimg/" + mangaDetail['im'] + "' alt='Card image cap'></center></a><div class='card-body'><a href='pages/manga.html?mangaID=" + mangaDetail['i'] + "' class='card-text'><center> " + mangaDetail['t'] + " </center></a></div></div></div>");
+}
+
+function createMangaForRandomPick(mangaDetail){
+  var element = document.getElementById('randomContent');
+  if (mangaDetail['im'] == null){
+    element.insertAdjacentHTML('afterbegin', "<div class='col-md-4'> <div class='card mb-4 box-shadow'><a href='pages/manga.html?mangaID=" + mangaDetail['i'] + "'><center><img class='card-img-top' src='img/notFound.png' alt='Card image cap'></center></a><div class='card-body'><a href='pages/manga.html?mangaID=" + mangaDetail['i'] + "' class='card-text'><center> " + mangaDetail['t'] + " </center></a></div></div></div>");
+
+  } else {
+    element.insertAdjacentHTML('afterbegin', "<div class='col-md-4'> <div class='card mb-4 box-shadow'><a href='pages/manga.html?mangaID=" + mangaDetail['i'] + "'><center><img class='card-img-top' src='https://cdn.mangaeden.com/mangasimg/" + mangaDetail['im'] + "' alt='Card image cap'></center></a><div class='card-body'><a href='pages/manga.html?mangaID=" + mangaDetail['i'] + "' class='card-text'><center> " + mangaDetail['t'] + " </center></a></div></div></div>");
+  }
 }
